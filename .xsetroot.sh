6331850e925e14c040cd242cc157ddb8a26b8317
John Jago <johnjago@protonmail.com>
2021-09-04 15:18:12 -0500

    Remove all symbols and icons from status bar

--- .xsetroot.sh
+++ .xsetroot.sh
@@ -1,5 +1,5 @@
-VOLUME=$(amixer sget Master | grep -e 'Front Left:' | sed 's/[^\[]*\[\([0-9]\{1,3\}%\).*\(on\|off\).*/\2 \1/' | sed 's/off/M/' | sed 's/on //')
-BATTERY=$(acpi -b | awk '{ print $4 }' | tr -d ',')
+VOLUME=$(amixer sget Master | grep -e 'Front Left:' | sed 's/[^\[]*\[\([0-9]\{1,3\}\).*\(on\|off\).*/\2 \1/' | sed 's/off/M/' | sed 's/on //')
+BATTERY=$(acpi -b | awk '{ print $4 }' | tr -d ',%')
 TIME=$(date +"%a %-d %b %R")
 
-xsetroot -name " 🔊 $VOLUME ❬ $BATTERY ❬ $TIME "
+xsetroot -name " $VOLUME ❬ $BATTERY ❬ $TIME "
