931c4b99dd2383136fb17ef82a9cdf9f999a9cc0
John Jago <johnjago@protonmail.com>
2021-09-03 23:53:25 -0500

    .xinitrc: set status bar text in another script
    This allows other programs to update the status bar immediately, and
    makes it so that I don't have to quit dwm and startx every time I want
    to update the status bar.

---
+++ .xsetroot.sh
@@ -0,0 +1,5 @@
+VOLUME=$(amixer sget Master | grep -e 'Front Left:' | sed 's/[^\[]*\[\([0-9]\{1,3\}%\).*\(on\|off\).*/\2 \1/' | sed 's/off/M/' | sed 's/on //')
+BATTERY=$(acpi -b | awk '{ print $4 }' | tr -d ',')
+TIME=$(date +"%a %-d %b %R")
+
+xsetroot -name " 🔊 $VOLUME ❬ $BATTERY ❬ $TIME "
