f17c9588ac8631e80ae14f0d551acab1906f293e
John Jago <johnjago@protonmail.com>
2021-09-05 13:56:37 -0500

    Always start dunst and improve battery notifications

--- .xsetroot.sh
+++ .xsetroot.sh
@@ -1,5 +1,18 @@
-VOLUME=$(amixer sget Master | grep -e 'Front Left:' | sed 's/[^\[]*\[\([0-9]\{1,3\}\).*\(on\|off\).*/\2 \1/' | sed 's/off/M/' | sed 's/on //')
+VOLUME=$(
+	amixer sget Master | \
+	grep -e 'Front Left:' | \
+	sed 's/[^\[]*\[\([0-9]\{1,3\}\).*\(on\|off\).*/\2 \1/' | \
+	sed 's/off/M/' | \
+	sed 's/on //'
+)
 BATTERY=$(acpi -b | awk '{ print $4 }' | tr -d ',%')
+STATUS=$(acpi -b | sed 's/.*: \([a-zA-Z]*\),.*/\1/gi')
 TIME=$(date +"%a %-d %b %R")
+
+if [ $BATTERY -lt 10 ] && [ $STATUS = 'Discharging' ]; then
+	BATTERY='Low battery'
+	# timeout is there so that notify-send doesn't hang if there's no daemon
+	timeout 2s notify-send 'Low battery'
+fi
 
 xsetroot -name " $VOLUME ❬ $BATTERY ❬ $TIME "
