454d6cb409e037adb81baa66a207fd5b8801e3cb
John Jago <johnjago@protonmail.com>
2021-09-05 14:54:12 -0500

    dwm: add a shortcut to reset gaps to default

--- dwm-6.2/config.h
+++ dwm-6.2/config.h
@@ -100,7 +100,8 @@ static Key keys[] = {
 	{ WINKEY,                       XK_Down,    spawn,         {.v = brightnessdowncmd } },
 	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
 	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
+	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0 } },
+	{ MODKEY|ShiftMask,             XK_minus,  setgaps,        {.i = gappx } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
