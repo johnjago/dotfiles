48bbb73b3254a081004a6b41356ede248ce220f5
John Jago <johnjago@protonmail.com>
2021-12-24 17:47:33 -0600

    Keep downloads and .git in backups

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 tar czvf /tmp/home_$(date -Idate).tar.gz \
-  --exclude=$HOME/downloads \
   --exclude=**/node_modules/* \
-  --exclude=**/.git/* \
   --exclude=**/.local/* \
   --exclude=**/.cache/* \
   --exclude=**/.npm/* \
