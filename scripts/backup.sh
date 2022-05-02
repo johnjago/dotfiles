639877f3377595efa538d9d2cb652f3d2080c4a4
John Jago <johnjago@protonmail.com>
2022-05-01 21:11:15 -0500

    Back up code and downloads separately

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
-tar czvf /tmp/home_$(date -Idate).tar.gz \
+# code and downloads have big files, so those will be compressed separately
+#tar czvf /tmp/home_$(date -Idate).tar.gz \
+#  --exclude=$HOME/code \
+#  --exclude=$HOME/downloads \
+#  --exclude=**/node_modules/* \
+#  --exclude=**/.local/* \
+#  --exclude=**/.cache/* \
+#  --exclude=**/.npm/* \
+#  --exclude=**/CacheStorage/* \
+#  --exclude=**/go/src/* \
+#  --exclude=**/go/pkg/* \
+#  --exclude=**/Cache/* \
+#  --exclude=**/.rustup/* \
+#  --exclude=**/.mozilla/* \
+#  $HOME
+
+tar czvf /tmp/home_code_$(date -Idate).tar.gz \
   --exclude=**/node_modules/* \
-  --exclude=**/.local/* \
-  --exclude=**/.cache/* \
-  --exclude=**/.npm/* \
-  --exclude=**/.gradle/* \
-  --exclude=**/cartinsight/* \
-  --exclude=**/CacheStorage/* \
-  --exclude=**/go/src/* \
-  --exclude=**/go/pkg/* \
-  --exclude=**/Cache/* \
-  --exclude=**/.rustup/* \
-  $HOME
+  $HOME/code
+
+tar czvf /tmp/home_downloads_$(date -Idate).tar.gz \
+  $HOME/downloads
